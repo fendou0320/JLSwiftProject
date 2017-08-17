8c823f32fb97bdab8be976c9a8fa709b3fefffab
甘草 <fendou0320@126.com>
2017-08-17 09:50:26 +0800

    Initial Commit

---
+++ JLSwiftProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JLSwiftProject
+//
+//  Created by UCS on 2017/8/17.
+//  Copyright © 2017年 www.ucsdigital.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
